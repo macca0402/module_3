76a98e3e5a022d3a5c0859925b52dd0575908eaf
macca0402 <yennhi0402dn@gmail.com>
2023-08-23 18:17:35 +0700

    23.8

---
+++ ss1/bai_tap/student_management.sql
@@ -0,0 +1,12 @@
+create table student_management.Class(
+id int not null,
+name varchar(45) null
+);
+create table student_management.teacher(
+id int not null,
+name varchar(45) null,
+age int not null,
+country varchar(45) null
+);
+select *from student_management.class;
+select *from student_management.teacher;
